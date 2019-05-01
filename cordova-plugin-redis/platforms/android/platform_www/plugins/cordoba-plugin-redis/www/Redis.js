baef45ba576c08ff1767aa8c3108804b94cb1633
evianyuji <33177149+evianyuji@users.noreply.github.com>
2019-05-01 12:23:06 +0900

    erase all

--- cordova-plugin-redis/platforms/android/platform_www/plugins/cordoba-plugin-redis/www/Redis.js
+++
@@ -1,8 +0,0 @@
-cordova.define("cordoba-plugin-redis.Redis", function(require, exports, module) {
-var exec = require('cordova/exec');
-
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'Redis', 'coolMethod', [arg0]);
-};
-
-});
